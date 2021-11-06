e1dac44d9153801e53a7053b8da862609407d8ed
SleepinWei <zyw0241@outlook.com>
2021-11-06 23:12:33 +0800

    make some changes
    - establish blueprint
    - add class User to models.py

---
+++ app/main/views.py
@@ -0,0 +1,29 @@
+from . import main
+import flask
+from models import User
+
+@main.route("/")
+def index():
+    # some functions to define hoempage of the website 
+    pass
+
+@main.route('/space')
+def spaceDefault():
+    return flask.render_template('DefaultSpace.html')
+
+# Default space
+@main.route('/space/')
+def spaceDefaultAddition():
+    return flask.render_template('DefaultSpace.html')
+
+# Redirect to user space
+@main.route('/space/<username>')
+def spaceUser(username):
+    # Wait background for query.
+    user1=User(username)
+    #user.FavouriteVideo[1].Name='666'
+    #user.FavouriteVideo[1].Information='232323232'
+    #user.FavouriteVideo[2].Name='777'
+    #user.FavouriteVideo[2].Information='7676767676'
+
+    return flask.render_template('UserSpace.html',user=user1)
