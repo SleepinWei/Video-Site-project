8f599d744a28226bb205d81fdc8d10c1577b6d51
tony <wutiancheng318@126.com>
2021-11-07 13:42:16 +0800

    add model_user

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
-class User:
-    #def __init__(username,NickName, ID, VIP,Level, LevelProgress,    Coins,    Stars,    Introduction):
-    def __init__(self,name):
-        self.NickName=name+'Nick'
-        self.ID=name+'ID'
-        self.VIP=True
-        self.Level=4
-        self.LevelProgress=30
-        self.Coins=10
-        self.Stars=4
-        self.Introduction="23333333"
-        self.FavouriteVideo=[{'name':'v1', 'Information':'i1john','Path':'none'},{'name':'v2', 'Information':'i2john','Path':'none'}]
+from app import db
+
+# class User:
+#    #def __init__(username,NickName, ID, VIP,Level, LevelProgress,    Coins,    Stars,    Introduction):
+#    def __init__(self,name):
+#        self.NickName=name+'Nick'
+#        self.ID=name+'ID'
+#        self.VIP=True
+#        self.Level=4
+#        self.LevelProgress=30
+#        self.Coins=10
+#        self.Stars=4
+#        self.Introduction="23333333"
+#        self.FavouriteVideo=[{'name':'v1', 'Information':'i1john','Path':'none'},{'name':'v2', 'Information':'i2john','Path':'none'}]
