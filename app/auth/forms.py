ff2730fd1f1e399db1ab5815b4e112b9e4dc8617
SleepinWei <zyw0241@outlook.com>
2021-11-22 19:34:05 +0800

    auth system
    - create login and register templates
    - update login system in auth/views.py however view functions for register hasn't been updated
    - update forms.py (login logout and register)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,25 @@
-from flask_wtf import Form
+from flask.app import Flask
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import Required,Length,Email
+from wtforms.validators import EqualTo, Regexp, Required,Length,Email, ValidationError
+from ..models import User
 
-class LoginForm(Form):
-    username = StringField("username",validators=[Required()])
+class LoginForm(FlaskForm):
+    name = StringField("username",validators=[Required()])
     password = PasswordField("Password",validators=[Required()])
     remember_me = BooleanField('Keep me logined in')
     submit = SubmitField("Login")
+
+class RegistrationForm(FlaskForm):
+    name = StringField('username',validators=[Required()\
+    ,Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,"username must only have letters,numbers,dots or underscores")])
+    password = PasswordField('Password',validators=[
+        Required(),EqualTo('password2',message="Passwords must match")
+    ])
+    password2 = PasswordField('Confirm password',validators=[Required()])
+    submit = SubmitField('Register')
+
+    def validate_username(self,field):
+        if User.query.filter_by(name=field.data).first():
+            raise ValidationError("Username already in use")
+        
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,24 @@
-from flask import render_template
+from flask import render_template,redirect,request,url_for,flash
 from . import auth
+from flask_login import login_user,logout_user,login_required
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login',methods=["GET","POST"])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.name.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        # 用户访问未授权URL会显示登录表单，FLASH将原地址保存在next参数中，因此能直接跳转
+        flash("Invalid username or password")
+    return render_template('auth/login.html',form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out")
+    return redirect(url_for("main.index"))
