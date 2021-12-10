7eed0cb8b465adb6af378a108727332bac6ebe6d
SleepinWei <zyw0241@outlook.com>
2021-12-10 14:18:59 +0800

    add new commands for uploading videos

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 import logging
 import click
-from app.models import User
+from app.models import User,Video
 from app import db
 
 def register(app):
@@ -9,4 +10,17 @@ def register(app):
     def create_user(username):
         user = User(name=username)
         db.session.add(user)
+        db.session.commit()
+    
+    @app.cli.command("add_video")
+    @click.argument("video_title")
+    @click.argument("video_info")
+    @click.argument("video_likenum")
+    def add_video(video_title,video_info,video_likenum):
+        video = Video(title=video_title,likenum=video_likenum,info=video_info)
+        video.url = "/video/"+video_title
+        video.thumbnail = "/static/images/thumbnail/"+video.title
+        video.uploadtime = datetime.utcnow()
+
+        db.session.add(video)
         db.session.commit()
