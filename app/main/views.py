e00f6ec8cb4c617af0e1d43be6b536ed53cf9d29
qwqOscar <87378223+qwqOscar@users.noreply.github.com>
2021-12-12 22:48:03 +0800

    hot fix: like coin and star are ok

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 from flask import request
 from werkzeug.urls import url_decode
 from werkzeug.utils import redirect
-from app.main.forms import CommentForm, EditProfileForm, RedirectToEditForm,createModule
+from app.main.forms import CommentForm, EditProfileForm, RedirectToEditForm, createModule, ButtonLike, ButtonCoin, ButtonStar, ButtonShare
 from . import main
 import flask
 # from ..models import User
@@ -59,16 +59,18 @@ def spaceUser(username):
         return flask.redirect(flask.url_for('editProfile'))
     return flask.render_template('UserSpace.html',user=current_user,form=form)
 
-@main.route('/video/<videoname>',methods=["POST","GET"])
+@main.route('/video/<videoname>',methods=["GET","POST"])
 def playvideo(videoname):
     # video1=Video(videoname)
     video = Video.query.filter_by(title=videoname).first()
     # 这里依据名字从查找video，后期可以改为依据id查找
 
     form = CommentForm()
-    buttonforms = createModule() #button forms 4 in 1 
+    buttonform1 = ButtonLike()
+    buttonform2 = ButtonCoin()
+    buttonform3 = ButtonStar()
+    buttonform4 = ButtonShare()
     # like,coin,star,share
-    
     if form.validate_on_submit():
         if current_user.is_anonymous or not current_user.is_authenticated:
             return redirect(url_for("auth.login"))
@@ -79,50 +81,61 @@ def playvideo(videoname):
         db.session.add(comment)
         return redirect(url_for('.playvideo'),videoname=videoname)
 
-    if buttonforms[0].validate_on_submit():
-        # like
-        exist_videolike = Videolike.query.filter_by(user_id=current_user.get_id(),video_id=video.id).first()
-        if exist_videolike == None:
-            video.likenum += 1 
-            videolike = Videolike(video_id=video.id,user_id=current_user.get_id())
-            db.session.add(videolike)
-        else:
-            video.likenum -= 1
-            db.session.delete(exist_videolike)
-        db.session.commit()
-
-    if(buttonforms[1].validate_on_submit()):
-        # coin
-        exist_videocoin = Videocoin.query.filter_by(user_id=current_user.get_id(),video_id=video.id).first()
-        if exist_videocoin == None:
-            video.coinnum += 1
-            videocoin = Videocoin(video_id=video.id,user_id=current_user.get_id())
-            db.session.add(videocoin)    
+    if request.method == 'GET':
+        if request.args.get('submit')=='Like' and flag[0]==0:
+            if current_user.is_anonymous or not current_user.is_authenticated:
+                return redirect(url_for("auth.login"))
+            exist_videolike = Videolike.query.filter_by(user_id=current_user.id,video_id=video.id).first()
+            if exist_videolike == None:
+                video.likenum += 1 
+                videolike = Videolike(video_id=video.id,user_id=current_user.id)
+                db.session.add_all([videolike,video])
+            else:
+                video.likenum -= 1
+                db.session.delete(exist_videolike)
+                db.session.add(video)
             db.session.commit()
-        else:
-            flash('You have throwed the coin')            
-
-    if(buttonforms[2].validate_on_submit()):
-        # star 
-        exist_videocol = Videocol.query.filter_by(user_id=current_user.get_id(),video_id=video.id).first()
-        if exist_videocol == None:
-            video.videocols += 1
-            videocol = Videocol(video.id,current_user.get_id())
-            db.session.add(videocol)
-        else:
-            db.session.delete(exist_videocol)
-        db.session.commit()
-
-    if(buttonforms[3].validate_on_submit()):
-        # share
-        pass
-        
-    
+            flag[0]+=1
+        elif request.args.get('submit')=='Like':
+            flag[0]-=1
+        elif request.args.get('submit')=='Coin' and flag[1]==0:
+            if current_user.is_anonymous or not current_user.is_authenticated:
+                return redirect(url_for("auth.login"))
+            exist_videocoin = Videocoin.query.filter_by(user_id=current_user.id,video_id=video.id).first()
+            if exist_videocoin == None:
+                video.coinnum += 1
+                current_user.coins -= 1
+                videocoin = Videocoin(video_id=video.id,user_id=current_user.id)
+                db.session.add_all([videocoin,video,current_user])    
+                db.session.commit()
+            else:
+                flash('You have throwed the coin')            
+            flag[1]+=1
+        elif request.args.get('submit')=='Coin':
+            flag[1]-=1
+        if request.args.get('submit')=='Star' and flag[2]==0:
+            if current_user.is_anonymous or not current_user.is_authenticated:
+                return redirect(url_for("auth.login"))
+            exist_videocol = Videocol.query.filter_by(user_id=current_user.id,video_id=video.id).first()
+            if exist_videocol == None:
+                video.videocols += 1
+                videocol = Videocol(video.id,current_user.id)
+                db.session.add_all([videocol,video])
+            else:
+                video.videocols -= 1
+                db.session.delete(exist_videocol)
+                db.session.add(video)
+            db.session.commit()
+            flag[2]+=1
+        elif request.args.get('submit')=='Star':
+            flag[2]-=1
+        elif request.args.get('submit')=='Coin':
+            pass
     comments = Comment.query.order_by(Comment.addtime.desc()).all()
     
     # return flask.render_template('extend.html',video=video1)
     return render_template('video.html',video=video,comments=comments, \
-    buttonforms=buttonforms,user=current_user,form=form)
+    buttonform1=buttonform1,buttonform2=buttonform2,buttonform3=buttonform3,buttonform4=buttonform4,user=current_user,form=form)
 
 # 用户资料编辑
 @main.route("/editProfile",methods=['GET','POST'])
